5abf99c79a1cf186fd1cc912285745ccd779f7db
JiYun <jo522115@gmail.com>
2022-11-15 23:23:52 +0900

    [Fix]: header&footer&3항조건식

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
+import MyHeader from "./MyHeader";
+import MyFooter from "./MyFooter";
 
 function App() {
   const name = "june";
+
+  const number = 5;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Welcome, {name}!</h2>
-      </header>
+      <MyHeader />
+      <h2>Welcome, {name}!</h2>
+      <b id="boldText">
+        {number}는 {number % 2 === 0 ? "짝수" : "홀수"}
+      </b>
+      <MyFooter />
     </div>
   );
 }

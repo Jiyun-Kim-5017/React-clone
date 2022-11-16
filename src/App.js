c4cbda48efe293f2fd422ce60d05057abb77d0c2
JiYun <jo522115@gmail.com>
2022-11-17 00:30:17 +0900

    [Feat]: DiaryEditor

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import "./App.css";
-import MyHeader from "./MyHeader";
-import MyFooter from "./MyFooter";
-import Counter from "./Counter";
-import Container from "./Container";
+import DiaryEditor from "./DiaryEditor";
 
 function App() {
-  const name = "june";
-  const number = 5;
-  const counterProps = {
-    a: 1,
-    b: 2,
-    c: 3,
-    initial: 5,
-  };
-
   return (
     <div className="App">
-      <Container>
-        <MyHeader />
-        <h2>Welcome, {name}!</h2>
-        <b id="boldText">
-          {number}는 {number % 2 === 0 ? "짝수" : "홀수"}
-        </b>
-        <Counter {...counterProps} />
-        <MyFooter />
-      </Container>
+      <DiaryEditor />
     </div>
   );
 }
---
+++ src/DiaryEditor.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({ name: "", content: "", emotion: 1 });
+  const nameInput = useRef();
+  const contentInput = useRef();
+
+  const handleChangeState = (e) => {
+    // console.log(state);
+    // console.log(e.target.value);
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (state.name.length < 1) {
+      alert("이름을 1글자 이상 입력해 주세요.");
+      nameInput.current.focus();
+      return;
+    }
+
+    if (state.content.length < 1) {
+      alert("내용을 1글자 이상 입력해 주세요.");
+      contentInput.current.focus();
+      return;
+    }
+
+    alert("저장 성공 :)");
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          ref={nameInput}
+          name="name"
+          value={state.name}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <span>오늘의 기분 : </span>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
